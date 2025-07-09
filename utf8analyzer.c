0dbe3525c7f75c94958e3755c1fb74c6ea08161e
Mayali <emayali@ieng6-203.ucsd.edu>
2025-07-09 12:58:13 -0700

    valid ascii and uppercased ascii added

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,6 +4,49 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+char* is_ascii(char string[]) {
+        // Implement is_ascii in the body of this function!
+        int result = 0;
+        for(int i = 0; i < strlen(string); i++){
+                if((string[i] & 0b10000000) == 0){
+                        result += 1;
+                }
+
+        }
+        if(result == strlen(string)){
+        return "true";
+        }
+        return "false";
+}
+
+
+int32_t capitalize_ascii(char str[]) {
+        // Implement capitalize_ascii in the body of this function!
+        int32_t count = 0;
+        for(int i = 0; i < strlen(str); i++){
+                if(str[i] >= 'a' && str[i] <= 'z'){
+                        int new = (str[i]-'0') + (int)16;
+                        str[i] = ((char)new);
+                        count++;
+                }
+
+        }
+        return count;
+}
+
+int get_str_length(const char *str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
@@ -11,4 +54,7 @@ int main(int argc, char *argv[]) {
     }
 
     // implement the UTF-8 analyzer here
+	printf("Valid ASCII: %s\n", is_ascii(argv[1]));
+	printf("Uppercased ASCI: %c\n", capitalize_ascii(argv[1]));
+
 }
